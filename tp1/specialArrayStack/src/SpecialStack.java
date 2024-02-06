307fa272bdb562a39850f91206e0b404914b979f
sixlandr2 <157863458+sixlandr2@users.noreply.github.com>
2024-02-06 18:34:05 -0500

    Update SpecialStack.java

--- tp1/specialArrayStack/src/SpecialStack.java
+++ tp1/specialArrayStack/src/SpecialStack.java
@@ -1,8 +1,8 @@
 public interface SpecialStack<E> {
-    public void push(E e);
-    public E pop();
-    public E top();
-    public boolean isEmpty();
-    public String toString();
+    public void push(E e);      //ajoute un élément sur la pile
+    public E pop();             //retire le dernier élément sur la pile et le renvoie
+    public E top();             //renvoie le dernier élément sur la pile
+    public boolean isEmpty();   //vérifie si la pile est vide
+    public String toString();   //produit une représentation en chaîne des éléments de la pile classés de haut en bas
 }
 
