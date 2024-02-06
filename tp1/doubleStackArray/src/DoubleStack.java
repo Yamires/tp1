e9d60f5baec1367042ac617319e77703fcc28294
sixlandr2 <157863458+sixlandr2@users.noreply.github.com>
2024-02-06 18:06:14 -0500

    Update DoubleStack.java

--- tp1/doubleStackArray/src/DoubleStack.java
+++ tp1/doubleStackArray/src/DoubleStack.java
@@ -1,7 +1,7 @@
 public interface DoubleStack<E> {
-    public void push(boolean one, E e);
-    public E pop(boolean one);
-    public E top(boolean one);
-    public  boolean isFull();
+    public void push(boolean one, E e);   //ajoute un élément sur la pile et renvoie vrai si possible
+    public E pop(boolean one);            //retire le dernier élément sur la pile et le renvoie
+    public E top(boolean one);            //renvoie la longueur de la pile
+    public  boolean isFull();             //vérifie si la pile double est pleine
 
 }
